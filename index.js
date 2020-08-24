b3323830a35d226bb1466b6e85aa3b1341d2f4c6
Omar Rodriguez <jrodriguez40@gmail.com>
2020-08-23 19:30:45 -0500

    first commit

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ app.use(cors())
 
 app.use(express.json({extend:true}))
 
+const port=process.env.port||4000
+
 app.use('/api/v1/users',require('./Rutas/Usuarios'));
 app.use('/api/v1/auth',require('./Rutas/auth'));
 app.use('/api/v1/proyectos',require('./Rutas/proyecto'))
 app.use('/api/v1/tareas',require('./Rutas/tareas'))
 
-app.listen(300,()=>{
+app.listen(port,'0.0.0.0',()=>{
     console.log('xd')
 })
 
